fad4c5a79104ef30f9168ba5b691ae429f39e954
Datcutepoy <dtcutepoy1708@gmail.com>
2026-01-23 06:51:34 +0700

    hello-world

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const AppRoot= ()=> {
+    return (
+        <View>
+            <Text>Hello world with expo router with datcutepoy</Text>
+        </View>
+    )
+}
+export default AppRoot;
