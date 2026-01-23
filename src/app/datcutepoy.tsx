642847a986a66a19a7727987e4352d1545e1b577
Datcutepoy <dtcutepoy1708@gmail.com>
2026-01-23 16:15:22 +0700

    bai57

---
+++ src/app/datcutepoy.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const Datcutepoy= ()=> {
+    return (
+        <View>
+            <Text>2 Datcutepoy</Text>
+        </View>
+    )
+}
+export default Datcutepoy;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
-
 const AppRoot= ()=> {
     return (
         <View>
             <Text>Hello world with expo router with datcutepoy</Text>
+            <Link href={"/datcutepoy"}>Go to datcutepoy</Link>
+            <Link href={"/like.tsx/like"}>Go to like</Link>
         </View>
     )
 }
