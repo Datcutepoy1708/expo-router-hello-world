e618954063ae3c0d87e4b5fb03264c6cfc893019
Datcutepoy <dtcutepoy1708@gmail.com>
2026-02-04 21:08:46 +0700

    base category

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,16 +1,18 @@
 import { Tabs } from "expo-router";
 
-const TabLayout = ()=> {
-    return (
-        <Tabs
-        screenOptions={{
-          headerShown:false,
-        }}
-        >
-          <Tabs.Screen name="index"/>
-          <Tabs.Screen name="settings" />
-        </Tabs>
-    )
+const TabLayout = () => {
+  return (
+    
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        sceneStyle: { backgroundColor: "#fff" }
+      }}
+    >
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="settings" />
+    </Tabs>
+  )
 }
 
 export default TabLayout;
