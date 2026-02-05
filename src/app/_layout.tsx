57ef26f8bb17a37988d2e88f658526ed5a1379b6
Datcutepoy <dtcutepoy1708@gmail.com>
2026-02-05 14:23:50 +0700

    use context

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import AppProvider from "@/context/app.context"
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native"
 import { Stack } from "expo-router"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
@@ -20,42 +21,44 @@ const RootLayout = () => {
     // </View>
     <GestureHandlerRootView>
       <RootSiblingParent>
-       <SafeAreaView style={{ flex: 1  }} >
-          <ThemeProvider value={navTheme}>
-            <Stack
-              screenOptions={{
-                headerStyle: {
-                  backgroundColor: "#f4511e"
-                },
-                headerTintColor: "#fff",
-                headerTitleStyle: {
-                  fontWeight: "bold"
-                },
-                // contentStyle:{backgroundColor: "#fff"}
-              }}
-            >
-              <Stack.Screen name="index" options={{ headerShown: false }} />
-              <Stack.Screen
-                name="(auth)/signup"
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="(auth)/verify"
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen
-                name="product/product"
-                options={{ headerTitle: "Sản phẩm" }}
-              />
-              {/* <Stack.Screen name="(auth)/login" options={{ headerTitle: "Đăng nhập" }} /> */}
-              <Stack.Screen
-                name="(auth)/login"
-                options={{ headerShown: false }}
-              />
-            </Stack>
-          </ThemeProvider>
-        </SafeAreaView>
+        <AppProvider>
+          <SafeAreaView style={{ flex: 1 }} >
+            <ThemeProvider value={navTheme}>
+              <Stack
+                screenOptions={{
+                  headerStyle: {
+                    backgroundColor: "#f4511e"
+                  },
+                  headerTintColor: "#fff",
+                  headerTitleStyle: {
+                    fontWeight: "bold"
+                  },
+                  // contentStyle:{backgroundColor: "#fff"}
+                }}
+              >
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen
+                  name="(auth)/signup"
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="(auth)/verify"
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen
+                  name="product/product"
+                  options={{ headerTitle: "Sản phẩm" }}
+                />
+                {/* <Stack.Screen name="(auth)/login" options={{ headerTitle: "Đăng nhập" }} /> */}
+                <Stack.Screen
+                  name="(auth)/login"
+                  options={{ headerShown: false }}
+                />
+              </Stack>
+            </ThemeProvider>
+          </SafeAreaView>
+        </AppProvider>
       </RootSiblingParent>
     </GestureHandlerRootView>
   )
